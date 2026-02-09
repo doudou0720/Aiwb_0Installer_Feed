76f387a86c21147f1661caa147c6308f0281eaa3
doudou0720 <98651603+doudou0720@users.noreply.github.com>
2026-02-09 21:03:00 +0800

    fix(ci):Remove double call to upx
    Signed-off-by: doudou0720 <98651603+doudou0720@users.noreply.github.com>

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 @echo off
 go build -ldflags "-s -w" -o go_wrapper.exe main.go
-where upx >nul 2>nul && upx -9 go_wrapper.exe
--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 go build -ldflags "-s -w" -o go_wrapper main.go
-command -v upx &> /dev/null && upx -9 go_wrapper
